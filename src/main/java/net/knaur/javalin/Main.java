29706058c796bd745791cac2503efc4ae5c8b1b9
tomek <>
2025-08-25 22:13:48 +0200

    Code refactor

--- src/main/java/net/knaur/javalin/Main.java
+++ src/main/java/net/knaur/javalin/Main.java
@@ -1,20 +1,21 @@
-package net.knaur.java;
+package net.knaur.javalin;
 
 import io.javalin.Javalin;
-import io.javalin.config.JavalinConfig;
 
 import static io.javalin.apibuilder.ApiBuilder.*;
 
-
 public class Main {
     public static void main(String[] args) {
+        Javalin app = createApp();
+        app.start(7000);
+    }
 
-        Javalin app = Javalin.create(cfg -> {
+    private static Javalin createApp() {
+        return Javalin.create(cfg -> {
             cfg.useVirtualThreads = true;
             cfg.router.apiBuilder(() -> {
                 get("/hello", ctx -> ctx.result("Hello World"));
             });
         });
-        app.start(7000);
     }
 }
